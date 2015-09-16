c8a7b1179771c4f79b39c590fe9c947099db2cd5
hennigk <kayla.hennig@gmail.com>
2015-09-16 23:14:14 +0000

    added cache function

---
+++ cached-location-server.js
@@ -0,0 +1,55 @@
+//for this to work properly the user must enter npm install on their
+//comand line
+//store cache in an array. 
+
+var http = require("http");
+var request = require('request');
+function storeCity(cityName, cityLat, cityLong) {
+    this.name = cityName;
+    this.lat = cityLat;
+    this.long = cityLong;
+}
+var cityArray = [];
+
+var server = http.createServer(function(req, response) {
+    response.writeHead(200);
+    if (req.url !== "/favicon.ico"){
+        var newCity = true;
+        var userCity = req.url.substring(1);
+        for (var i =0; i < cityArray.length; i++){
+            if (userCity.toLowerCase() === cityArray[i].name) {
+                newCity = false;
+                response.write(" \n Your city is: " + cityArray[i].name) ;
+                response.write(" \n Your latitude is: " + cityArray[i].lat) ;
+                response.write("\n Your longitude is: " + cityArray[i].long);
+                response.end();
+            } 
+        }
+        if (newCity){
+            var httpRequest = "https://maps.googleapis.com/maps/api/geocode/json?address=" + userCity + "&key=AIzaSyCyJgwsFhRRPWDik2jba7YRWU45soLDtwY";
+            request(httpRequest, function(err, res, body) {
+                if (err) {
+                    response.end('Sorry, try again! There was an error:' + err);           
+                }
+                var data = JSON.parse(body);
+                var firstResult = data.results[0];
+            
+                if (firstResult) {
+                    var lat1 = data.results[0].geometry.location.lat;
+                    var long1 = data.results[0].geometry.location.lng;
+                    cityArray.push(new storeCity(userCity.toLowerCase(), lat1, long1));
+                    response.write(" \n Your city is: " + userCity) ;
+                    response.write(" \n Your latitude is: " + lat1.toString()) ;
+                    response.write("\n Your longitude is: " + long1.toString());
+                    response.end();
+                }
+                else {
+                    response.write('Sorry there are no results');
+                    response.end();    
+                }
+            });
+        }    
+    }
+});
+
+server.listen(process.env.PORT, process.env.IP);

4e891fd3c7e11938ff4fbb24e396360fd878fe40
hennigk <kayla.hennig@gmail.com>
2015-09-16 19:48:34 +0000

    removed comments on fortune-teller.js

--- fortune-teller.js
+++ fortune-teller.js
@@ -1,5 +1,6 @@
 var randomFortune = require("./library/fortune.js");
 
+//check to see if an element was given in the command line
 if (process.argv[2] !== undefined) {
     console.log("You selected to receive " + process.argv[2] + " fortunes!"); 
     console.log(""); //nicer output of the fortunes
@@ -11,26 +12,3 @@ if (process.argv[2] !== undefined) {
 }
 
 console.log(randomFortune());
-
-
-/*// print process.argv
-
-process.argv.forEach(function(val, index, array) {
-  console.log(index + ': ' + val);
-});
-
-
-This will generate:
-
-$ node process-2.js one two=three four
-
-0: node
-1: /Users/mjr/work/node/process-2.js
-2: one
-3: two=three
-4: four
-
-Try to get that last 5 command-line argument from the process.argv array
-Using this argument, output the number of fortunes requested by the user
-
-*/

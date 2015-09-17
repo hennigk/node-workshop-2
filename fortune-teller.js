3e980c456e1fec2027cfbe196d6d360cbd94924a
hennigk <kayla.hennig@gmail.com>
2015-09-17 15:33:27 +0000

    used \n to add line spaces instead of console.log()

--- fortune-teller.js
+++ fortune-teller.js
@@ -4,11 +4,9 @@ var randomFortune = require("./library/fortune.js");
 //check to see if an element was given in the command line
 if (process.argv[2] !== undefined) {
     console.log("You selected to receive " + process.argv[2] + " fortunes! \n"); 
-    //console.log(""); //nicer output of the fortunes
     
     for (var i=0; i <= process.argv[2]; i++) {
         console.log(randomFortune() + "\n");
-        //console.log(""); //nicer output of the fortunes
     }
 }
 
